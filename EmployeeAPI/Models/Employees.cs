292a54c056d1d6339d54be0a3e97e43abed85f0a
chandrapragathish <chandrapragathish@gmail.com>
2021-03-22 14:55:54 +0530

    First

---
+++ EmployeeAPI/Models/Employees.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmployeeAPI.Models
+{
+    public class Employees
+    {
+        public string EmployeeName { get; set; }
+        public string EmployeeCode { get; set; }
+        public string EmployeeEmail { get; set; }
+        public string EmployeeTP { get; set; }
+    }
+}
